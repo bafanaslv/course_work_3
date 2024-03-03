298e7eb7ea243f445d645fb0f3aaf0bfbc4f12f3
Вячеслав Лукин <bafana.slv@gmail.com>
2024-03-03 17:24:03 +0300

    Sorted operation list.

--- modules/main.py
+++ modules/main.py
@@ -5,6 +5,7 @@ import os.path
 import json
 import os
 from os.path import dirname
+from operator import itemgetter
 
 OPERATION_JSON_FILE = os.path.join(dirname(os.getcwd()), 'data', 'operations.json')
 
@@ -29,18 +30,23 @@ class Operation:
         """Возвращает сумму перевода."""
         return self.amount
 
+#    def sorting(self):
+#        return sorted(self, key=lambda x: x['date'], reverse=True)
+
+
 
 def create_operation_list(path):
     """Получаем список operations__list из json-файла.
     Создаем список объектов operations__objects из экземпляров класса Operation."""
     operations_list = load_json_file(path)
+#    sorted_list = sorted(operations_list, key=itemgetter('date'), reverse=True)
     operations_objects = []
     for op_object in operations_list:
         if len(op_object) > 0 and op_object["state"] == 'EXECUTED':
-            print(op_object)
-            o = Operation(op_object.get("id"), op_object.get("state"), op_object.get("date"),
-            op_object.get("operationAmount"), op_object.get("description"), op_object.get("from"), op_object.get("to"))
-            operations_objects.append(o)
+#            print(op_object)
+ #           o = Operation(op_object.get("id"), op_object.get("state"), op_object.get("date"),
+ #          op_object.get("operationAmount"), op_object.get("description"), op_object.get("from"), op_object.get("to"))
+            operations_objects.append(op_object)
     return operations_objects
 
 
@@ -58,8 +64,12 @@ def load_json_file(path):
 
 def main():
     operations_objects = create_operation_list(OPERATION_JSON_FILE)
-    for operation in operations_objects:
-        print(operation.get_amount())
+#    sorted_list = sorted(operations_objects, key=itemgetter('date'), reverse=True)
+    sorted_list = sorted(operations_objects, key=lambda x: x['date'], reverse=True)
+#    for operation in operations_objects:
+#        print(operation.get_amount())
+    for operation in sorted_list:
+        print(operation)
 
 
 if __name__ == '__main__':
