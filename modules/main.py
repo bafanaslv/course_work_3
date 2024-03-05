30016172dd82b70014c5dde1a25a1e1ed0c82faa
Вячеслав Лукин <bafana.slv@gmail.com>
2024-03-05 09:09:52 +0300

    Сортировка платежей и вывод результов.

--- modules/main.py
+++ modules/main.py
@@ -8,14 +8,70 @@ from os.path import dirname
 
 OPERATION_JSON_FILE = os.path.join(dirname(os.getcwd()), 'data', 'operations.json')
 
-def create_operation_list(path):
-    """Получаем список operations__list из json-файла.
-    Создаем список объектов operations__objects из экземпляров класса Operation."""
+
+class Operation:
+    """id_ - идентификатор, state - состояние, date - дата операции, amount - сумма операции,
+     description - описание, payer - плательщик, receiver - получатель"""
+
+    def __init__(self, id_, state, date, amount, description, payer, receiver):
+        self.id = id_
+        self.state = state
+        self.date = date
+        self.amount = amount
+        self.description = description
+        self.payer = payer
+        self.receiver = receiver
+
+    def get_date(self):
+        """Возвращает дату операции перевода в классическом формате."""
+        rus_date = self.date[:10].split("-")[::-1]
+        return '.'.join(rus_date)
+
+    def get_description(self):
+        """Возвращает описание операции."""
+        return self.description
+
+    def get_payer(self):
+        """Возвращает плательщика."""
+        return self.payer
+
+    def get_receiver(self):
+        """Возвращает получателя."""
+        return self.receiver
+
+    def get_amount(self):
+        """Возвращает сумму операции."""
+        currency = self.amount["currency"]
+        return self.amount["amount"]+' '+currency["name"]
+
+
+def mask_card(card_account):
+    """Возвращает номера карты или счета в маске."""
+    if card_account[:4] == 'Счет':
+        # здесь и так понятно :)
+        return "Счет **"+card_account[-4:]
+    else:
+        # делим название карты и номер на список, разделяем номер карты по четыре номера и маскируем номер
+        # символам '*' затем соединяем части наименования карты с его замакированным номером.
+        card = card_account.split(" ")
+        len_card = len(card_account.split())
+        card_name = ' '.join(card[0:len_card - 1])+' '
+        card_number = card[-1]
+        private_number = card_number[:6] + (len(card_number[6:-4]) * '*') + card_number[-4:]
+        chunks, chunk_size = len(private_number), len(private_number) // 4
+        return card_name + " ".join([private_number[i:i + chunk_size] for i in range(0, chunks, chunk_size)])
+
+
+def create_operation_objects(path):
+    """Получаем список operations_list из json-файла.
+    Создаем список объектов operations_objects из экземпляров класса Operation."""
     operations_list = load_json_file(path)
     operations_objects = []
     for op_object in operations_list:
         if len(op_object) > 0 and op_object["state"] == 'EXECUTED':
-            operations_objects.append(op_object)
+            o = Operation(op_object.get("id"), op_object.get("state"), op_object.get("date"),
+            op_object.get("operationAmount"), op_object.get("description"), op_object.get("from"), op_object.get("to"))
+            operations_objects.append(o)
     return operations_objects
 
 
@@ -25,17 +81,33 @@ def load_json_file(path):
         print(f'Файл {path} не существует или указан неверный путь к нему !\n')
         exit()
     elif os.stat(path).st_size == 0:
-        print('Файл пустой - тест прерван !')
+        print('Файл пустой - продолжение невозможно !')
         exit()
     with open(path, 'r', encoding='utf-8') as file:
         return json.load(file)
 
 
 def main():
-    operations_objects = create_operation_list(OPERATION_JSON_FILE)
-    operations_objects.sort(key=lambda x: x['date'], reverse=True)
-    for operation in operations_objects:
-        print(operation)
+    """create_operation_objects создает список объектов операций, сортирует список по дата (x.date)
+    и выводит результаты операций в требуемомо виде."""
+    operations_objects = create_operation_objects(OPERATION_JSON_FILE)
+    operations_objects.sort(key=lambda x: x.date, reverse=True)
+    # i - счетчик операций
+    # quantity - количество элементов списка которое нужно обработат
+    i = 0
+    quantity = 5
+    while i <= quantity - 1:
+        # метод get_date() класса Operation выводит дату в формате dd.mm.yyyy, get_description - описание операции
+        # метод get_payer() выводит счет или карту плательщика, get_receiver() - получателя.
+        # функция mask_card() перед выводом накладывает маску (*) на часть номера карты или счета.
+        # метод get_amount() выводит сумму баковской операции.
+        print(operations_objects[i].get_date()+' '+operations_objects[i].get_description())
+        if operations_objects[i].get_payer() is None:
+            print(mask_card(operations_objects[i].get_receiver()))
+        else:
+            print(mask_card(operations_objects[i].get_payer())+' -> '+mask_card(operations_objects[i].get_receiver()))
+        print(f'{operations_objects[i].get_amount()}\n')
+        i += 1
 
 
 if __name__ == '__main__':
